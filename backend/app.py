3b3693570ad2d19c1fd215d4ea5c7fb3c28282f7
ThetaLog(n) <giorgiomartucci02@gmail.com>
2025-09-10 18:34:59 +0200

    update

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,13 @@
 from flask import Flask, jsonify, send_from_directory
+
+
+
 app = Flask(__name__, static_folder='../frontend')
 
-# Servire frontend
 @app.route('/')
 def index():
     return send_from_directory(app.static_folder, 'index.html')
 
-# API semplice
 @app.route('/api/data')
 def api_data():
     return jsonify({
